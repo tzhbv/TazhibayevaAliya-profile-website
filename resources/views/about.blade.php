072a678640e0a680b192be41a4370f48bd91c129
tzhbv <tzhbv@users.noreply.github.com>
2021-03-16 03:59:22 +0600

    initial commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,280 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initiale-scale=1">
+<title>Resume</title>
+
+<link rel="stylesheet" type="text/css" href="Portfolio.css"/>
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@500;700&family=Karla:wght@700&family=Lexend+Peta&family=Lobster&family=Mukta:wght@600&family=Tajawal&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css"/>
+
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Andika+New+Basic&display=swap" rel="stylesheet">
+
+
+</head>
+
+	
+</br>
+</br>
+</br>
+</br>
+</br>
+		<section id="about"> 
+	<h5 style="text-align:center; font-family: 'Lexend Peta', sans-serif; color:black">ABOUT ME  </h5>
+	<p style="text-align:center; font-family: 'Lora', serif; color:grey; font-size: 22px;">Hi! My name is Aliya.</p>
+	<p style="text-align:center; font-family: 'Lora', serif; color:grey; font-size: 22px;"> I am a Programmer & Developer from Kazakhstan, Almaty.</p>
+	
+	
+	<div class="flexx">
+	<div class="fullname">
+	<h4 style="letter-spacing:2px;">FULLNAME:</h4>
+	<p style="font-family: 'Andika New Basic', sans-serif; margin-top:-20px;">Tazhibayeva Aliya Orynbassarovna</p>
+	<h4 style="letter-spacing:2px; ">BIRTH   DATE:  </h4>
+	<p style="font-family: 'Andika New Basic', sans-serif; margin-top:-20px;">July 22,2002</p>
+	<h4 style="letter-spacing:2px;">ADDRESS:</h4>
+	<p style="font-family: 'Andika New Basic', sans-serif; margin-top:-20px;">Kazalhstan, Almaty</p>
+	<h4 style="letter-spacing:2px;">LANGUAGE:</h4>
+	<p style="font-family: 'Andika New Basic', sans-serif; margin-top:-20px;">English, Kazakh, Russian, Turkish, Korean</p>
+	<h4 style="letter-spacing:2px;">STATUS:</h4>
+	<p style="font-family: 'Andika New Basic', sans-serif; margin-top:-20px;">Single</p>
+	<h4 style="letter-spacing:2px;">EMAIL:</h4>
+	<p style="font-family: 'Andika New Basic', sans-serif; margin-top:-20px;">tzhbv@gmail.com</p>
+	</div>
+	
+		<div class="mfullname">
+	<h4 style="letter-spacing:2px;">FULLNAME:</h4>
+	<p style="font-family: 'Andika New Basic', sans-serif; margin-top:-20px;">Tazhibayeva Aliya Orynbassarovna</p>
+	<h4 style="letter-spacing:2px; ">BIRTH   DATE:  </h4>
+	<p style="font-family: 'Andika New Basic', sans-serif; margin-top:-20px;">July 22,2002</p>
+	<h4 style="letter-spacing:2px;">ADDRESS:</h4>
+	<p style="font-family: 'Andika New Basic', sans-serif; margin-top:-20px;">Kazalhstan, Almaty</p>
+	<h4 style="letter-spacing:2px;">LANGUAGE:</h4>
+	<p style="font-family: 'Andika New Basic', sans-serif; margin-top:-20px;">English, Kazakh, Russian, Turkish, Korean</p>
+	<h4 style="letter-spacing:2px;">STATUS:</h4>
+	<p style="font-family: 'Andika New Basic', sans-serif; margin-top:-20px;">Single</p>
+	<h4 style="letter-spacing:2px;">EMAIL:</h4>
+	<p style="font-family: 'Andika New Basic', sans-serif; margin-top:-20px;">tzhbv@gmail.com</p>
+	</div>
+	
+
+	<div class="skills0" >
+	<div class="skills" >
+	<img src="https://i.stack.imgur.com/rYn2b.png?s=328&g=1" width="120px;" height="120px;" style="border-radius:25px;"/>
+	<img src="https://amazingcss.com/logos/logo-1024x1024.png" width="120px;" height="120px;" style="border-radius:25px;"/>
+	</div>
+	<div class="skills2" >
+	<img src="https://i.ya-webdesign.com/images/png-fonts-free-download-7.png" width="120px;" height="120px;" style=""/>
+	<img src="https://is3-ssl.mzstatic.com/image/pf/us/r30/Purple3/v4/37/bc/0c/37bc0cf5-521f-2c1d-d3ee-a88c017de202/mzl.jexgttoc.png" width="120px;" height="120px;" style="border-radius:25px;"/>
+	</div>
+	</div>
+	</div>
+	</section>
+	
+	</br>
+	</br>
+	<div class="contactme" >
+	<a href="#contact" title="Contact Me" style="width: 12%; padding:10px;line-height: 1.5rem;" class="button stroke smoothscroll" onclick="contact()">Contact Me</a>
+    <script>
+            function contact() {
+            location.replace("https://labka/contact")
+            }
+    </script>
+	</div>
+    <br>
+	
+	</section>
+	
+	<h4 style="font-family:'Cormorant Garamond', serif; text-align:center;"><strong>© tzhbv. 2020</strong></h4>
+	
+<style>
+
+	.skills0{
+	float:right;
+	margin-left:200px;
+	margin-top:70px;
+	}
+	.flexx{
+	display: flex;
+	}
+
+	.all{
+	text-align: center;
+	font-size:20px;
+	}
+	
+
+	.education{
+	 text-align: center;
+	}
+	.popup{
+    top: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+	background-color:black;
+	height: 100%;
+    width: 100%;
+	}
+
+	.popup-content{
+   	background-color: ;
+    height: 400px;
+    width: 500px;
+    position: relative;
+    padding: 10px;
+    border-radius: 10px;
+    text-align: center;
+    }
+	
+	input{
+    display: block;
+    margin: 22px auto;
+    width: 60%;
+	height:3%;
+    padding: 15px;
+	}
+	.contact{
+	text-align: center;
+	}
+	
+	html,body{
+	margin:0;
+	padding:0;
+	width:100%;
+	overflow-x:hidden;
+	scroll-behavior:smooth;
+	}
+	body{
+	background-color:#f6f5f5;
+	margin:0;
+	padding:0;
+	background:#EDEBEC;
+	}
+	.black{
+	width:100%;
+	height:400px;
+	background-color:black;
+	}
+	.todark{
+	height:400px;
+	width:100%;
+	background:black;
+	position:absolute;
+	top:0px;
+	text-align:center;
+	}
+
+	.texts{
+	position:relative;
+	top:-70px;
+	}
+	.names{
+	color:white;
+	font-family: 'Lexend Peta', sans-serif;
+	font-size:45px;
+	}
+	.progdev{
+	color:white;
+	font-family: 'Lexend Peta', sans-serif;
+	}
+	.Moreabout{
+	text-align: center;
+	}
+	.contactme{
+	text-align: center;
+	}
+
+	.fullname{
+	margin-left:280px;
+	}
+.intro-content .button {
+    border-color: rgba(255, 255, 255, 0.3);
+
+    font-size: 20px;
+    text-transform: uppercase;
+	text-align: center;
+}
+button.stroke, .button.stroke {
+    background: transparent !important;
+    border: 3px solid #313131;
+    line-height: 3.5rem;
+
+}
+.button, a.button, button, input[type="submit"], input[type="reset"], input[type="button"] {
+	display: inline-block;
+    font-family: "poppins-bold", sans-serif;
+    font-size: 20px;
+    text-transform: uppercase;
+    letter-spacing: 0.2rem;
+    height: 1.1rem;
+    line-height: 1.4rem;
+    padding: 0 1rem;
+	padding-bottom: 35px;
+    margin-bottom:10px ;
+    background: #d8d8d8;
+    color: #313131;
+    text-decoration: none;
+    cursor: pointer;
+    text-align: center;
+    white-space: nowrap;
+    border: none;
+}
+	@media screen and (max-width:900px) {
+	body{
+	overflow-x:hidden;
+	}
+	.fullname{
+	display:none;
+	}
+	.mfullname{
+	text-align:center;
+	margin-left:50px;
+	}
+	.skills0{
+	display:none;
+	}
+	.Moreabout{
+	display:none;
+	}
+	.contactme{
+	display:none;
+	}
+	.texts{
+	display:none;
+	}
+	.me{
+	display:none;
+	}
+	.mtexts{
+	display:inline;
+	}
+	}
+	
+	@media screen and (min-width:900px) {
+	.Moreabout{
+	text-align: center;
+	}
+	.mfullname{
+	display:none;
+	}
+	.contactme{
+	text-align: center;
+	}
+	.texts{
+	display:inline;
+	}
+	.me{
+	display:inline;
+	}
+	.mtexts{
+	display:none;
+	}
+	}
+</style>
+
+
+	</body>
+	</html>
---
+++ resources/views/contact.blade.php
@@ -0,0 +1,238 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initiale-scale=1">
+<title>Resume</title>
+
+<link rel="stylesheet" type="text/css" href="Portfolio.css"/>
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@500;700&family=Karla:wght@700&family=Lexend+Peta&family=Lobster&family=Mukta:wght@600&family=Tajawal&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css"/>
+
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Andika+New+Basic&display=swap" rel="stylesheet">
+
+
+</head>
+
+	
+
+	<section id="education"> 
+	<div class="all">
+	<h5  style="text-align:center; font-family: 'Lexend Peta', sans-serif; color:black; font-size:30px;border-bottom:4px solid black;">Education </h5>
+	
+	<p style="font-family: 'Andika New Basic', sans-serif;"><strong>School 2014-2019 :</strong>  Almaty "Bilim-Innovation" high school for girls </p>
+
+	<p style="font-family: 'Andika New Basic', sans-serif;"><strong>2019-2023 :</strong> Suleyman Demirel University</p>
+
+	<p style="font-family: 'Andika New Basic', sans-serif;"><strong>Specialty :</strong> Information Systems</p>
+	</div>
+	</section>
+
+	<section id="contact"> 
+		<div class="popup">
+        <div class="popup-content">
+		</br>
+          <h1 style="font-family:'Cormorant Garamond', serif; color:white; ">Contact me</h1>
+		  <input id="email" type="text" name="username" placeholder="Email address"  style="font-family:'Cormorant Garamond', serif; font-size:18px;">
+		  <input id="name" type="text" name="name" placeholder="Name"  style="font-family:'Cormorant Garamond', serif; font-size:18px;">
+		<input type="tel" id="phone" name="phone" placeholder="123-45-678">
+		</br> <button  class="btn btn-outline-dark"   style="font-family:'Cormorant Garamond', serif;font-size:18px; background-color:white;line-height: 2.3rem;"><strong> Send</strong></button>
+
+   </div>
+    </div>
+	
+	</section>
+	
+	<h4 style="font-family:'Cormorant Garamond', serif; text-align:center;"><strong>© tzhbv. 2020</strong></h4>
+	
+<style>
+
+	.skills0{
+	float:right;
+	margin-left:200px;
+	margin-top:70px;
+	}
+	.flexx{
+	display: flex;
+	}
+
+	.all{
+	text-align: center;
+	font-size:20px;
+	}
+	
+
+	.education{
+	 text-align: center;
+	}
+	.popup{
+    top: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+	background-color:black;
+	height: 100%;
+    width: 100%;
+	}
+
+	.popup-content{
+   	background-color: ;
+    height: 400px;
+    width: 500px;
+    position: relative;
+    padding: 10px;
+    border-radius: 10px;
+    text-align: center;
+    }
+	
+	input{
+    display: block;
+    margin: 22px auto;
+    width: 60%;
+	height:3%;
+    padding: 15px;
+	}
+	.contact{
+	text-align: center;
+	}
+	
+	html,body{
+	margin:0;
+	padding:0;
+	width:100%;
+	overflow-x:hidden;
+	scroll-behavior:smooth;
+	}
+	body{
+	background-color:#f6f5f5;
+	margin:0;
+	padding:0;
+	background:#EDEBEC;
+	}
+	.black{
+	width:100%;
+	height:400px;
+	background-color:black;
+	}
+	.todark{
+	height:400px;
+	width:100%;
+	background:black;
+	position:absolute;
+	top:0px;
+	text-align:center;
+	}
+
+	.texts{
+	position:relative;
+	top:-70px;
+	}
+	.names{
+	color:white;
+	font-family: 'Lexend Peta', sans-serif;
+	font-size:45px;
+	}
+	.progdev{
+	color:white;
+	font-family: 'Lexend Peta', sans-serif;
+	}
+	.Moreabout{
+	text-align: center;
+	}
+	.contactme{
+	text-align: center;
+	}
+
+	.fullname{
+	margin-left:280px;
+	}
+.intro-content .button {
+    border-color: rgba(255, 255, 255, 0.3);
+
+    font-size: 20px;
+    text-transform: uppercase;
+	text-align: center;
+}
+button.stroke, .button.stroke {
+    background: transparent !important;
+    border: 3px solid #313131;
+    line-height: 3.5rem;
+
+}
+.button, a.button, button, input[type="submit"], input[type="reset"], input[type="button"] {
+	display: inline-block;
+    font-family: "poppins-bold", sans-serif;
+    font-size: 20px;
+    text-transform: uppercase;
+    letter-spacing: 0.2rem;
+    height: 1.1rem;
+    line-height: 1.4rem;
+    padding: 0 1rem;
+	padding-bottom: 35px;
+    margin-bottom:10px ;
+    background: #d8d8d8;
+    color: #313131;
+    text-decoration: none;
+    cursor: pointer;
+    text-align: center;
+    white-space: nowrap;
+    border: none;
+}
+	@media screen and (max-width:900px) {
+	body{
+	overflow-x:hidden;
+	}
+	.fullname{
+	display:none;
+	}
+	.mfullname{
+	text-align:center;
+	margin-left:50px;
+	}
+	.skills0{
+	display:none;
+	}
+	.Moreabout{
+	display:none;
+	}
+	.contactme{
+	display:none;
+	}
+	.texts{
+	display:none;
+	}
+	.me{
+	display:none;
+	}
+	.mtexts{
+	display:inline;
+	}
+	}
+	
+	@media screen and (min-width:900px) {
+	.Moreabout{
+	text-align: center;
+	}
+	.mfullname{
+	display:none;
+	}
+	.contactme{
+	text-align: center;
+	}
+	.texts{
+	display:inline;
+	}
+	.me{
+	display:inline;
+	}
+	.mtexts{
+	display:none;
+	}
+	}
+</style>
+
+
+	</body>
+	</html>
