b2d2c72bb0698c66b2cbac84d602797fc1036477
tzhbv <75580060+tzhbv@users.noreply.github.com>
2021-03-19 01:37:17 +0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -33,13 +33,3 @@ Route::get('/contact', function () {
 Route::get('/home', function () {
     return view('index');
 });
-Route::get('post/create', function(){
-    DB::table('post')->insert([
-       'tittle' => 'Aliya Tazhibayeva',
-       'body' => 'Second course student of Suleyman Demirel University , major is IT.'
-    ]);
-});
-Route::get('post', function(){
-    $post=Post::find(1);
-    return $post->body;
- });
